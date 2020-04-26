7307fc5c105350dda39f7a5980d39e9ef704a23c
David NoMa <davidnm@gmail.com>
2020-04-26 21:50:55 +0100

    15 commit

--- chartfunctions.js
+++ chartfunctions.js
@@ -57,7 +57,7 @@ const searchCompany = async searchBox => {
   
     if (searchBox.length === 0) {
       fits = [];
-      companyList.innerHTML = '';
+      document.getElementById('countryList').innerHTML = '';
     }
   
     outputHtml(fits);
@@ -66,23 +66,10 @@ const searchCompany = async searchBox => {
   // show results in HTML
   const outputHtml = fits => {
     if (fits.length > 0) {
-      const html = fits
-        .map(
+      const html = fits.map(
           fit => `
        <div class="row">
-       <div class="col s12">
-         <div class="card  grey darken-4 darken-1">
-           <div class="card-content white-text">
-             <h4 class="card-title m1">${fit.Company_Name} (${
-            fit.Company_Market_Cap
-          })<span class="blue-text m-4"> ${fit.Country_of_Incorporation}</span></h4>
-          <div class="card-action">
-          <a>Country Code :</a>
-          <a>${fit.Market}</a>
-        </div>
-           </div>
-         </div>
-       </div>
+       <a href="./shares.html?share=${fit.Company_Name}">${fit.Company_Name} (${fit.ICB_Industry})</a>
      </div>
        `
         )
